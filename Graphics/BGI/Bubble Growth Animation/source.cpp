f3ce8f1cadc94148de70ca43dcfc7837834b9c91
Jatin Khandual <39887790+hitch-hiker42@users.noreply.github.com>
2020-10-27 20:11:57 +0530

    Update source.cpp

--- Graphics/BGI/Bubble Growth Animation/source.cpp
+++ Graphics/BGI/Bubble Growth Animation/source.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-  initwindow(500, 500, "House");
+  initwindow(500, 500, "Bubble");
   int left = 10, right = 100, r = left, count = 0, x = getmaxx() >> 1, y = getmaxy() >> 1, n = 10;
   while(true) {
     for(int i = left; i <= right; i += 10) circle(x, y, i), delay(100), cleardevice();
